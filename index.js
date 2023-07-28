c95c4b9759df833fa066d03c71a43be284688509
Matthew York <myorkprogramming@gmail.com>
2023-07-27 23:52:03 -0700

    Flatten

--- index.js
+++ index.js
@@ -9,3 +9,29 @@ Alter order if needed. Ex.: [1,[2,3],4,[]] could become [1,3,4,2].
 
 Third: make your algorithm both in-place and stable. 
 Do you need a return value? */
+
+function flatten(arr){
+    var newArr = [];
+    for(var i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+            for(var j = 0; j < arr[i].length; j++){
+                newArr.push(arr[i][j]);
+            }
+        }
+        else{
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+
+//Example:
+console.log(flatten([1,[2,3],4,[]]));
+
+//We set a new array to push the values into.
+//We loop through the array.
+//If the value is an array, we loop through that array and push the values into the new array.
+//If the value is not an array, we push that value into the new array.
+//We return the new array.
+
+//#########################################################################################################
