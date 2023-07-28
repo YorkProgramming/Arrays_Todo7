fae89a97180e901767e399fc237f84e74bf6331c
Matthew York <myorkprogramming@gmail.com>
2023-07-27 23:50:22 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//1.
+//Flatten
+
+/* Flatten a given array, eliminating nested and empty arrays. 
+Do not alter it; return a new one retaining order. For [1,[2,3],4,[]] return [1,2,3,4].
+
+Second: work ‘in-place’ in the given array (do not create another). 
+Alter order if needed. Ex.: [1,[2,3],4,[]] could become [1,3,4,2].
+
+Third: make your algorithm both in-place and stable. 
+Do you need a return value? */
