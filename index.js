4ea4bf42ebd58b6cb74d5818f6fdbe2951c64f50
Matthew York <myorkprogramming@gmail.com>
2023-07-30 01:13:58 -0700

    Mode

--- index.js
+++ index.js
@@ -70,3 +70,15 @@ console.log(removeDuplicates([1,2,1,3,4,2]));
 //We return the new array.
 
 //#########################################################################################################
+
+//3.
+//Mode
+
+/* Back in the Basic 13, you wrote code to compute an array’s minimum and maximum values. 
+You also wrote code to determine the average value (the “mean”). 
+What about the “mode” – the most common value in that data set. 
+Create a function that, given an array, returns the most frequent value in the array.
+
+Second: memory constraints prevent your using a new array. 
+How does this affect your solution?
+ */
