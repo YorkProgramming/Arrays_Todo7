4ecdf8e98674db7b4da0f9ad81ddf413ca624ce5
Matthew York <myorkprogramming@gmail.com>
2023-07-29 23:10:10 -0700

    Remove Duplicates

--- index.js
+++ index.js
@@ -35,3 +35,18 @@ console.log(flatten([1,[2,3],4,[]]));
 //We return the new array.
 
 //#########################################################################################################
+
+//2.  
+//Remove Duplicates
+
+/* Remove array duplicates. Do not alter original. 
+Return new array with results ‘stable’ (original order). 
+For [1,2,1,3,4,2] return [1,2,3,4].
+
+Second: work ‘in-place’ in given array. 
+Alter order if needed (stability is not required). 
+Ex.: [1,2,1,3,4,2] could become [1,2,4,3].
+
+Third: make it in-place and stable.
+
+Fourth: eliminate any second (inner) loop. */
