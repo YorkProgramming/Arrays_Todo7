f517f22c95e0ce100d72c4207afee22053f76ece
Matthew York <myorkprogramming@gmail.com>
2023-08-01 22:49:54 -0700

    Buffer Example

--- index.js
+++ index.js
@@ -164,3 +164,12 @@ function arrBufferCopy(sourceArr,destArr,sourceStartIdx,destStartIdx,numVals){
 }
 
 //Example:
+console.log(arrBufferCopy([1,2,3,4,5,6,7,8,9],[10,11,12,13,14,15,16,17,18],2,4,3));
+
+//We set a count variable to 0.
+//We loop through the source array starting at the sourceStartIdx.
+//If count is less than numVals, we set the value of the source array equal to the value of the dest array.
+//We increment destStartIdx and count.
+//We return the dest array.
+
+//#########################################################################################################
